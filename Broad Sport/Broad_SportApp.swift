b32855dc3dff118bd3412b4f53f987f942295ff2
Radcabna <radcabna@gmail.com>
2026-03-25 09:02:54 +0300

    Initial Commit

---
+++ Broad Sport/Broad_SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Broad_SportApp.swift
+//  Broad Sport
+//
+//  Created by Алкександр Степанов on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Broad_SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
